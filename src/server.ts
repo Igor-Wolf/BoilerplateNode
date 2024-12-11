23659b018f64b563217409cc93acf4b54ee06950
Igor <igorreisbarbosa@gmail.com>
2024-12-11 18:55:20 -0300

    initial

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+console.log("ola mundo")
+
+
+//===================== Dependencias
+
+//instalar dependencias já configuradas
+//npm install
+
+//depois adicionar:
+
+// No terminal execute os comandos do express
+// npm i express
+
+
+// cors
+// // npm i cors
+
+
+//===================== Criar o arquivo
+
+//.env que provavelmente será apagado devido a configurações do .gitignore
+
+
+//===================== Separar os arquivos de acordo com a necessidade
+
+//server.ts
+//routes.ts
+//app.ts
+
+//===================== Ao final verificar a versão de distribuição que será postada na vercel
